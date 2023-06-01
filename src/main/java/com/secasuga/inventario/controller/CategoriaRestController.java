472faf2d7b0c7334590f2778e566eb057e3fe83c
sebastian <secasuga@gmail.com>
2023-05-31 20:37:33 -0500

    commit para el cross en el controller

--- src/main/java/com/secasuga/inventario/controller/CategoriaRestController.java
+++ src/main/java/com/secasuga/inventario/controller/CategoriaRestController.java
@@ -1,6 +1,7 @@
 package com.secasuga.inventario.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import com.secasuga.inventario.model.Categoria;
 import com.secasuga.inventario.response.CategoriaResponseRest;
 import com.secasuga.inventario.servicios.InterfazCategoriaServicios;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoriaRestController {
